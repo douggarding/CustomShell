365d9f8fba8ea441e3e79a6bd57f680a326e9de5
Doug <doug.garding@outlook.com>
2018-02-08 17:40:41 -0700

    finished part 3, piping seems to be working

--- main.cpp
+++ main.cpp
@@ -10,48 +10,72 @@
 #include <unistd.h> // for execvp
 #include "shelpers.hpp"
 
-int main(int argc, const char * argv[]) {
-    
+#define READ_END 0
+#define WRITE_END 1
+
+int main(int argc, const char *argv[])
+{
+
     std::string userCommand;
-    
+
     // Running loop that allows for user to input their commands
-    while(std::getline(std::cin, userCommand)){
-        
+    while (std::getline(std::cin, userCommand))
+    {
+
         // Turn the user input into command(s)
         std::vector<std::string> tokens = tokenize(userCommand);
         std::vector<Command> commands = getCommands(tokens);
 
         // Check for exit command
-        if("exit" == commands[0].exec){
+        if ("exit" == commands[0].exec)
+        {
             return 0;
         }
 
-        // TODO: check if a real command exists. Then fork.
-        int pid = fork();
-        
-        if(pid < 0){
-            std::cout << "fork() failed. Exiting shell program.\n";
-            return 0;
-        }
+        // Process each of the commands
+        for (int i = 0; i < commands.size(); i++){
+            // TODO: check if a real command exists. Then fork.
+            int pid = fork();
 
-        else if(pid == 0) { // CHILD PROCESS
-            int working = 0;
-            working = execvp(commands[0].argv[0], const_cast<char* const*>(commands[0].argv.data()));
-
-            // Check for errors with exec command
-            if (working < 0){
-                std::cout << "execvp failed with error: " << errno << "\n";
-                exit(errno);
+            if (pid < 0)
+            {
+                std::cout << "fork() failed. Exiting shell program.\n";
+                return 0;
             }
-        
-        }
-        else { // PARENT PROCESS
-            wait(NULL);
-        }
 
-        
+            else if (pid == 0)
+            { // CHILD PROCESS
+
+                // Set up correct file descriptors.
+                dup2(commands[i].fdStdin, READ_END);
+                dup2(commands[i].fdStdout, WRITE_END);
+
+                int working = 0;
+                working = execvp(commands[i].argv[0], const_cast<char *const *>(commands[i].argv.data()));
+
+                // Check for errors with exec command
+                if (working < 0)
+                {
+                    std::cout << "execvp failed with error: " << errno << "\n";
+                    exit(errno);
+                }
+            }
+            else
+            { // PARENT PROCESS
+
+                // Close read end of pipe if used
+                if(commands[i].fdStdin > 0){
+                    close(commands[i].fdStdin);
+                }
+                // Close write end of pipe if used
+                if(commands[i].fdStdout > 1){
+                    close(commands[i].fdStdout);
+                }
+
+                wait(NULL); 
+            }
+        }
     }
-    
 
     std::cout << "PROGRAM TERMINATED!\n";
     return 0;
